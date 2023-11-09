c74812bcb6d8917ed1e4bd239e6c88a5580ba019
kesucs12345 <keshucscs@gmail.com>
2023-11-09 10:48:47 +0530

    gracefully shotdown

--- main.go
+++ main.go
@@ -1,17 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Message struct {
+	From     string
+	Playload string
+}
+
+type Server struct {
+	msgChan  chan Message
+	quitchan chan struct{}
+}
+
+func (s *Server) StartAndListen() {
+
+	for {
+		select {
+		case msg := <-s.msgChan:
+			fmt.Printf("received message from: %s payload: %s\n", msg.From, msg.Playload)
+		case <-s.quitchan:
+			fmt.Println("Server is gracefully shut down")
+			return
+		}
+	}
+}
+
+func SendMessageToServer(msgCh chan Message, payload string) {
+	fmt.Println("Sending the message")
+	msg := Message{
+		From:     "Keshav",
+		Playload: payload,
+	}
+	msgCh <- msg
+}
 
 func main() {
+	s := &Server{
+		msgChan:  make(chan Message),
+		quitchan: make(chan struct{}),
+	}
+	go s.StartAndListen()
 
-	ch := make(chan any)
+	for i := 1; i < 10; i++ {
+		go func() {
+			time.Sleep(10 * time.Microsecond)
+			SendMessageToServer(s.msgChan, "Hello server")
+		}()
+	}
 
-	go func() {
-		val, ok := <-ch
+	time.Sleep(1 * time.Second) // Wait for a while
+	// select {}
 
-		fmt.Println(val, ok)
-	}()
-	ch <- "hello"
-	close(ch)
-
+	close(s.quitchan)                  // Gracefully shut down the server by closing the quitchan
+	time.Sleep(100 * time.Millisecond) // Give the server some time to process the quit signal
 }
